445a38af2479e4547141a2f7e4a2824a2ac80d61
nico <nicodinicola9@icloud.com>
2022-05-23 00:06:38 -0700

    Mastered the button

--- app/src/main/java/com/wadf/triviagame/AboutPage.java
+++ app/src/main/java/com/wadf/triviagame/AboutPage.java
@@ -2,7 +2,10 @@ package com.wadf.triviagame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import triviagame.R;
 
@@ -12,5 +15,13 @@ public class AboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+        Button button = (Button) findViewById(R.id.aboutback);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //pat code here
+                Intent intent = new Intent(AboutPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
