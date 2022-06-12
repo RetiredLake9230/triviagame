2da1bf4e23429c6a143f1d84fce77d4a180c567b
RetiredLake9230 <nicodinicola@hotmail.com>
2022-06-11 22:45:14 -0700

    Revert "Getting lazy.."
    This reverts commit 05c556be5c1847f7d2e41323a3252046a42e46a9.

--- app/src/main/java/com/wadf/triviagame/MainActivity.java
+++ app/src/main/java/com/wadf/triviagame/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,26 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1 = (Button) findViewById(R.id.about);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.about);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //pat code here
                 Intent intent = new Intent(MainActivity.this, AboutPage.class);
                 startActivity(intent);
             }
         });
-        Button btn2 = (Button) findViewById(R.id.startkey);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //pat code here
-                Intent intent = new Intent(MainActivity.this, AboutPage.class);
-                startActivity(intent);
-                //toast
-                String message = "Starting now.. Try your best!";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(MainActivity.this, message, duration);
-                toast.show();
-            }
-});
     }
 }
